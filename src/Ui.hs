3fa90fe441a1729239ff4f2965a68b0a4f39cc05
Denys Lytviak <deh4er@gmail.com>
2020-03-26 17:16:16 +0100

    reformat ui

--- src/Ui.hs
+++ src/Ui.hs
@@ -6,6 +6,14 @@ import qualified Core
 import qualified Core.Utils as Utils
 
 
+startUi :: Core.App -> IO ()
+startUi app = do
+  cfg <- standardIOConfig
+  vty <- mkVty cfg
+  ui vty app
+  shutdown vty
+
+
 ui :: Vty -> Core.App -> IO ()
 ui vty app =
   unless (Core.isAppClosed app) $ do
@@ -22,26 +30,55 @@ ui vty app =
 
 
 mapEvent :: Event -> Maybe Core.Event
-mapEvent (EvKey (KChar s) [MCtrl]) = Just Core.saveEvent
-mapEvent (EvKey (KChar c) _) = Just . Core.keyEvent $ Core.keyChar c
-mapEvent (EvKey KUp _) = Just . Core.keyEvent $ Core.keyUp
-mapEvent (EvKey KDown _) = Just . Core.keyEvent $ Core.keyDown
-mapEvent (EvKey KLeft _) = Just . Core.keyEvent $ Core.keyLeft
-mapEvent (EvKey KRight _) = Just . Core.keyEvent $ Core.keyRight
-mapEvent (EvKey KEnter _) = Just . Core.keyEvent $ Core.keyEnter
-mapEvent (EvKey KBS _) = Just . Core.keyEvent $ Core.keyBackspace
-mapEvent (EvKey KEsc _) = Just Core.closeEvent
-mapEvent _ = Nothing
+mapEvent (EvKey (KChar s) [MCtrl]) =
+  Just Core.saveEvent
+
+mapEvent (EvKey (KChar c) _) =
+  Just . Core.keyEvent $ Core.keyChar c
+
+mapEvent (EvKey KUp _) =
+  Just . Core.keyEvent $ Core.keyUp
+
+mapEvent (EvKey KDown _) =
+  Just . Core.keyEvent $ Core.keyDown
+
+mapEvent (EvKey KLeft _) =
+  Just . Core.keyEvent $ Core.keyLeft
+
+mapEvent (EvKey KRight _) =
+  Just . Core.keyEvent $ Core.keyRight
+
+mapEvent (EvKey KEnter _) =
+  Just . Core.keyEvent $ Core.keyEnter
+
+mapEvent (EvKey KBS _) =
+  Just . Core.keyEvent $ Core.keyBackspace
+
+mapEvent (EvKey KEsc _) =
+  Just Core.closeEvent
+
+mapEvent _ =
+  Nothing
 
 
 makePicture :: Core.App -> DisplayRegion -> Picture
 makePicture app (width, height) = picForImage croppedImage
  where
-  lines = Core.getLines app
-  buffer = Core.getBuffer app
-  cursor = Core.getCursor buffer
-  (row, col) = Core.getRowCol cursor
-  image = makeImage cursor lines
+  lines =
+    Core.getLines app
+
+  buffer =
+    Core.getBuffer app
+
+  cursor =
+    Core.getCursor buffer
+
+  (row, col) =
+    Core.getRowCol cursor
+
+  image =
+    makeImage cursor lines
+
   croppedXImage =
     if col >= width then
       translateX (width - col - 1) image
@@ -56,7 +93,9 @@ makePicture app (width, height) = picForImage croppedImage
 
 
 makeImage :: Core.Cursor -> [String] -> Image
-makeImage cursor lines = linesImage row col lines
+makeImage cursor lines =
+  linesImage row col lines
+
   where
     row :: Core.Row
     row = Core.getRow cursor
@@ -66,9 +105,15 @@ makeImage cursor lines = linesImage row col lines
 
 
 linesImage :: Core.Row -> Core.Col -> [String] -> Image
-linesImage 0 col (x:xs) = lineCursorImage col x <-> linesImage (-1) col xs
-linesImage _ _ [] = emptyImage
-linesImage row col (x:xs) = string attr x <-> linesImage (row - 1) col xs
+linesImage 0 col (x:xs) =
+  lineCursorImage col x <-> linesImage (-1) col xs
+
+linesImage _ _ [] =
+  emptyImage
+
+linesImage row col (x:xs) =
+  string attr x <-> linesImage (row - 1) col xs
+
 
 
 lineCursorImage :: Core.Col -> String -> Image
@@ -79,27 +124,25 @@ lineCursorImage col x =
     _ ->
       before <|> cursor <|> after
   where
-    before = string attr (take col x)
+    before =
+      string attr (take col x)
+
     cursor =
       if col == length x
         then
           char cursorAttr ' '
         else
           char cursorAttr (x !! col)
-    after = string attr (drop (col + 1) x)
+    after =
+      string attr (drop (col + 1) x)
 
 
 attr :: Attr
-attr = defAttr
+attr =
+  defAttr
 
 
 cursorAttr :: Attr
-cursorAttr = defAttr `withForeColor` black `withBackColor` white
+cursorAttr =
+  defAttr `withForeColor` black `withBackColor` white
 
-
-startUi :: Core.App -> IO ()
-startUi app = do
-  cfg <- standardIOConfig
-  vty <- mkVty cfg
-  ui vty app
-  shutdown vty

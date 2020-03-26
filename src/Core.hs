0ac5d293b55bb198b69c15f1361723516510af2e
Denys Lytviak <deh4er@gmail.com>
2020-03-26 13:55:22 +0100

    rename Core.Core module to Core.App

--- src/Core.hs
+++ src/Core.hs
@@ -1,12 +1,12 @@
 module Core (
-  module Core.Core,
+  module Core.App,
   module Core.Fs,
   module Core.Event,
   module Core.Buffer,
   module Core.Cursor
 ) where
 
-import Core.Core
+import Core.App
 import Core.Fs
 import Core.Event
 import Core.Buffer

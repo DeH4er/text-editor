ded05aecdf549ba2b5c28bac500aee022668801f
Denys Lytviak <deh4er@gmail.com>
2020-03-31 17:11:00 +0200

    refactoring of Window

--- src/Core/Window.hs
+++ src/Core/Window.hs
@@ -54,9 +54,88 @@ getBuffer =
   winBuffer
 
 
+getContent :: Window -> [String]
+getContent =
+  Buffer.getContent . getBuffer
+
+
 getAllCursors :: Window -> [Cursor]
 getAllCursors window =
   winMainCursor window : winCursors window
+
+
+modifyContent :: ([String] -> [String]) -> Window -> Window
+modifyContent f =
+  modifyBuffer $ Buffer.modifyContent f
+
+
+modifyContentByCursors :: ([Cursor] -> [String] -> [String]) -> Window -> Window
+modifyContentByCursors f window =
+  modifyContent (f $ getAllCursors window) window
+
+
+modifyGroupedCursors :: ([[Cursor]] -> [[Cursor]]) -> Window -> Window
+modifyGroupedCursors f =
+  modifyCursors $ concat . f . groupCursors
+
+
+modifyCursorsByContent :: ([String] -> [Cursor] -> [Cursor]) -> Window -> Window
+modifyCursorsByContent f window =
+  modifyCursors (f $ getContent window) window
+
+
+fitViewByMainCursor :: Window -> Window
+fitViewByMainCursor window =
+  fitViewByCursor (getMainCursor window) window
+
+
+fitViewByCursor :: Cursor -> Window -> Window
+fitViewByCursor = modifyRect . doMoveView
+  where
+    doMoveView :: Cursor -> Rect -> Rect
+    doMoveView cursor rect = Rect.new newRow newCol rWidth rHeight
+      where
+        newRow
+          | row >= (rRow + rHeight - 1) =
+            row - rHeight + 1
+          | row < rRow =
+            row
+          | otherwise =
+            rRow
+
+        newCol
+          | col >= (rCol + rWidth - 1) =
+            col - rWidth + 1
+          | col < rCol =
+            col
+          | otherwise =
+            rCol
+
+        rRow = Rect.getRow rect
+        rCol = Rect.getCol rect
+        rWidth = Rect.getWidth rect
+        rHeight = Rect.getHeight rect
+        (row, col) = Cursor.getRowCol cursor
+
+
+getGroupedCursors :: Window -> [[Cursor]]
+getGroupedCursors = groupCursors . getAllCursors
+
+
+groupCursors :: [Cursor] -> [[Cursor]]
+groupCursors = sortGroupedCursors . doGroupCursors
+  where
+    sortGroupedCursors :: [[Cursor]] -> [[Cursor]]
+    sortGroupedCursors [] = []
+    sortGroupedCursors (rowCursors : others) =
+      sortOn Cursor.getCol rowCursors : sortGroupedCursors others
+
+    doGroupCursors :: [Cursor] -> [[Cursor]]
+    doGroupCursors =
+      groupBy byRows
+        where
+          byRows :: Cursor -> Cursor -> Bool
+          byRows cursor1 cursor2 = Cursor.getRow cursor1 == Cursor.getRow cursor2
 
 
 getMainCursor :: Window -> Cursor
@@ -135,72 +214,25 @@ modifyCursors f window =
 
 
 moveCursors :: MoveAction -> Window -> Window
-moveCursors action window =
-  withNewRect
+moveCursors action =
+  fitViewByMainCursor . modifyCursorsByContent doMoveCursors
     where
-      withNewRect :: Window
-      withNewRect = modifyRect (doMoveView $ getMainCursor withNewCursors) withNewCursors
-
-      withNewCursors :: Window
-      withNewCursors = modifyCursors doMoveCursors window
-
-      doMoveCursors :: [Cursor] -> [Cursor]
-      doMoveCursors cursors =
+      doMoveCursors :: [String] -> [Cursor] -> [Cursor]
+      doMoveCursors content cursors =
         moveCursor action content <$> cursors
-
-      doMoveView :: Cursor -> Rect -> Rect
-      doMoveView cursor rect = Rect.new newRow newCol rWidth rHeight
-        where
-          newRow
-            | row >= (rRow + rHeight - 1) =
-              row - rHeight + 1
-            | row < rRow =
-              row
-            | otherwise =
-              rRow
-
-          newCol
-            | col >= (rCol + rWidth - 1) =
-              col - rWidth + 1
-            | col < rCol =
-              col
-            | otherwise =
-              rCol
-
-          rRow = Rect.getRow rect
-          rCol = Rect.getCol rect
-          rWidth = Rect.getWidth rect
-          rHeight = Rect.getHeight rect
-          (row, col) = Cursor.getRowCol cursor
-
-      content :: [String]
-      content = Buffer.getContent . getBuffer $ window
 
 
 insertChar :: Char -> Window -> Window
-insertChar char window =
-  withMovedCursors
+insertChar char =
+  fitViewByMainCursor . withMovedCursors . withInsertedChar
     where
-      withMovedCursors :: Window
-      withMovedCursors = modifyCursors doMove withInsertedChar
+      withMovedCursors :: Window -> Window
+      withMovedCursors =
+        modifyGroupedCursors moveGroupedCursors
 
-      withInsertedChar :: Window
-      withInsertedChar = modifyBuffer (doInsert $ getAllCursors window) window
-
-      doInsert :: [Cursor] -> Buffer -> Buffer
-      doInsert = Buffer.modifyContent . insertAtCursors
-
-      doMove :: [Cursor] -> [Cursor]
-      doMove = concat . moveGroupedCursors . sortGroupedCursors . groupCursors
-
-      moveGroupedCursors :: [[Cursor]] -> [[Cursor]]
-      moveGroupedCursors = fmap $ mapIndex doMapIndex
-
-      doMapIndex :: Int -> Cursor -> Cursor
-      doMapIndex i = Cursor.modify $ doModify i
-
-      doModify :: Int -> (Cursor.Row, Cursor.Col) -> (Cursor.Row, Cursor.Col)
-      doModify i (row, col) = (row, col + i + 1)
+      withInsertedChar :: Window -> Window
+      withInsertedChar =
+        modifyContentByCursors insertAtCursors
 
       insertAtCursors :: [Cursor] -> [String] -> [String]
       insertAtCursors [] content = content
@@ -213,17 +245,14 @@ insertChar char window =
           where
             (row, col) = Cursor.getRowCol cursor
 
-      sortGroupedCursors :: [[Cursor]] -> [[Cursor]]
-      sortGroupedCursors [] = []
-      sortGroupedCursors (rowCursors : others) =
-        sortOn Cursor.getCol rowCursors : sortGroupedCursors others
+      moveGroupedCursors :: [[Cursor]] -> [[Cursor]]
+      moveGroupedCursors = fmap $ mapIndex doMapIndex
 
-      groupCursors :: [Cursor] -> [[Cursor]]
-      groupCursors =
-        groupBy byRows
-          where
-            byRows :: Cursor -> Cursor -> Bool
-            byRows cursor1 cursor2 = Cursor.getRow cursor1 == Cursor.getRow cursor2
+      doMapIndex :: Int -> Cursor -> Cursor
+      doMapIndex i = Cursor.modify $ doModify i
+
+      doModify :: Int -> (Cursor.Row, Cursor.Col) -> (Cursor.Row, Cursor.Col)
+      doModify i (row, col) = (row, col + i + 1)
 
 
 moveCursor :: MoveAction -> [String] -> Cursor -> Cursor
